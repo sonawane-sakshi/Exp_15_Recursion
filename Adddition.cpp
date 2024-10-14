6c03694e59a2186b8c32c8809be8cc7ef67f63be
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-14 20:01:32 +0530

    Add files via upload

---
+++ Adddition.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+
+int sum(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return n + sum(n - 1);
+}
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Sum of integers is: " << sum(num) << endl;
+    return 0;
+}
